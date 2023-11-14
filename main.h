3bd6b56dd14966cdd8747e7877a40b26df73cd25
Crownezt <adepheranmie@gmail.com>
2023-11-14 16:45:07 +0100

    new change

--- main.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINTF_HEADER
-#define PRINTF_HEADER
-#include <stdarg.h>
-int _putchar(char c);
-int _printf(const char *format, ...);
-int _print_char(va_list args);
-int _print_string(va_list args);
-#endif
