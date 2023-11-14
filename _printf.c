4900d571ca90117441f540234d71e95408fbe55e
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-14 03:50:58 -0800

    Implements printf

--- _printf.c
+++ _printf.c
@@ -1,3 +1,6 @@
+#include <stdargs.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -8,5 +11,43 @@
 
 int _printf(const char *format, ...)
 {
-	
+	va_list characters;
+
+	va_start(characters, format);
+
+	int counter = 0;
+	int i;
+
+	while (format[counter] != '\0')
+	{
+		if (format[counter] == '%')
+		{
+			if (format[counter + 1] == 'c')
+			{
+				i = _print_char(characters);
+				if (i < 0)
+				{
+					return (-1);
+				}
+			}
+			else if(format[counter + 1] == 's')
+			{
+				i = print_string(characters);
+				if (i < 0)
+				{
+					return (-1);
+				}
+			}
+			else if (!format[counter + 1])
+			{
+				return (-1);
+			}
+			counter++;
+		} else
+		{
+			_putchar(format[counter]);
+		}
+		counter++;
+	}
+	return (0);
 }
