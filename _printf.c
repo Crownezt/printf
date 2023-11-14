2e39b2f17ef046d3a046d64a5a90bb41bcbecca5
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-11-14 03:02:56 -0800

    adds printf

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _printf - print a formatted string
+* @format: the formatted string
+* Return: the number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	
+}
